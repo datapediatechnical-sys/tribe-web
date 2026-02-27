cae804779bda2c255ee1fe86c4cd2b28a1fa0be8
Divya147940 <Divyanshiverma484@gmail.com>
2026-02-27 18:18:09 +0530

    Add GitHub Pages deployment: workflow, HashRouter, base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import WeddingEvents from './pages/WeddingEvents'
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/products" element={<Products />} />
         <Route path="/programs" element={<Programs />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
